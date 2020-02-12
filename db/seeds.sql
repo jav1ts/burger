66e7cbc248aec1b6309c38c02dad1afd409959e4
jav1ts <55659287+jav1ts@users.noreply.github.com>
2020-02-12 00:51:01 -0500

    initial setup files

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('Double Bacon Cheeseburger');
+INSERT INTO burgers (name) VALUES ('Crabby Patty');
+INSERT INTO burgers (name, devoured) VALUES ('Super Slider', true);
+INSERT INTO burgers (name, devoured) VALUES ('Mysterious Vegan Surprise', true);
+INSERT INTO burgers (name, devoured) VALUES ('Gummy Bear Burger', true);
+INSERT INTO burgers (name) VALUES ('Mickey Dz');
